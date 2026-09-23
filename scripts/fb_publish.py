d21a0c4cea024dc975cf861764d3d62e8b9003a5
vuchanphong6758bit <vuchanphong6758@gmail.com>
2026-09-23 15:17:36 +0700

    fix: use current Facebook Graph API version

--- scripts/fb_publish.py
+++ scripts/fb_publish.py
@@ -4,7 +4,7 @@ page_id=os.environ['FACEBOOK_PAGE_ID']
 token=os.environ['FACEBOOK_PAGE_ACCESS_TOKEN']
 message=os.environ['MESSAGE']
 link=os.environ['LINK']
-url=f'https://graph.facebook.com/v23.0/{page_id}/feed'
+url=f'https://graph.facebook.com/v26.0/{page_id}/feed'
 r=requests.post(url,data={'message':message,'link':link,'access_token':token},timeout=60)
 if not r.ok:
     raise SystemExit(f'Facebook publish failed: HTTP {r.status_code} {r.text}')
